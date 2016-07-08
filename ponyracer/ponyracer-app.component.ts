34a267df566d4829b14471648cc2e40dd1abbb31
Aybou <abou.bacarkercodebysimplon@gmail.com>
2016-07-08 16:01:59 +0200

    npm install

--- ponyracer/ponyracer-app.component.ts
+++ ponyracer/ponyracer-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'ponyracer-app'
+    selector: 'ponyracer-app',
+    template : '<h1>PonyRacer</h1>' 
 })
     
 export class PonyRacerAppComponent {
