81e842b5a11f51f327e55ddcf0d3471fd3514990
Aybou <abou.bacarkercodebysimplon@gmail.com>
2016-07-08 15:28:50 +0200

    creation bootstrap.ts

---
+++ ponyracer/bootsrap.ts
@@ -0,0 +1,6 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { PonyRacerAppComponent } from './ponyracer-app.Component';
+
+bootstrap( PonyRacerAppComponent )
+    .catch(err => console.console.log(err);
+    )
